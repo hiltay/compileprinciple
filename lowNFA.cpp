c6756c3b2cb8a0ea621a8f208b3a7e504b336397
yyyz <904108079@qq.com>
2023-01-04 16:06:43 +0800

    nfa matrix console print;

--- lowNFA.cpp
+++ lowNFA.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <iomanip>
 
 using namespace std;
 
@@ -25,6 +28,7 @@ typedef struct VertexNode {
 
 typedef struct NFA {
     int start, end;
+    string seen_char;
     VertexNode Graph[];
 } NFA;
 
@@ -79,15 +83,23 @@ private:
     }
 
 
-    string convert_postfix_exp(string infix_exp) {
+    pair<string, set<char>> convert_postfix_exp(string infix_exp) {
         // see:https://zh.m.wikipedia.org/zh-hans/%E8%B0%83%E5%BA%A6%E5%9C%BA%E7%AE%97%E6%B3%95
+        // 输出结果为后缀表达式
         string postfix_exp;
+        // 辅助栈，存放操作符
         stack<char> operators;
+        // 当前的字符
         char cur_elem;
+        // 保存这个正则表达式输入中出现的所有字符
+        set<char> seen_char;
+
         for (int i = 0; i < infix_exp.size(); i++) {
             cur_elem = infix_exp[i];
             // 字符数字下划线
             if (is_character(cur_elem)) {
+                // 添加到set，用于后续展示nfa使用，和构造后缀表达式的逻辑无关
+                seen_char.insert(cur_elem);
                 if (i != 0) {
                     // 当前为普通字符，且非首个字符，查看上一个字符是否为普通字符或者除了左括号以外的特殊字符
                     // 例如：aa )a *a +a .a ?a
@@ -141,6 +153,9 @@ private:
                 // 删掉栈中的(
                 operators.pop();
             } else if (is_special_character(cur_elem)) {
+                // 通配符.在展示时视作普通字符，所以需要添加到set，用于后续展示nfa使用，和构造后缀表达式的逻辑无关
+                if (cur_elem == '.')
+                    seen_char.insert(cur_elem);
                 // 特殊运算符*.?+|
                 while (!operators.empty() && priority(cur_elem) <= priority(operators.top())) {
                     // 比较当前字符和运算符栈顶字符的优先级，如果当前字符的优先级小
@@ -163,22 +178,15 @@ private:
         }
         // 至此得到后缀表达式
         cout << "输入的中缀表达式：" << infix_exp << "\n对应的后缀表达式：" << postfix_exp << endl;
-        return postfix_exp;
+        return make_pair(postfix_exp, seen_char);
     }
-
-//    void update_graph(NFA* nfa,VertexNode basic_node_start,VertexNode basic_node_end,int * start_index,int * end_index,stack<pair<int, int>> assist) {
-//        nfa->Graph[basic_node_start.index] = basic_node_start;
-//        nfa->Graph[basic_node_end.index] = basic_node_end;
-//        start_index = &basic_node_start.index;
-//        end_index = &basic_node_end.index;
-//        assist.push(make_pair(start_index, end_index));
-//    }
 
 public:
 
     NFA *construct(string re) {
         // 得到后缀表达式
-        string postfix_exp = convert_postfix_exp(re);
+        pair<string, set<char>> result = convert_postfix_exp(re);
+        string postfix_exp = result.first;
         // 确定最大状态数，以分配邻接表内存
         auto max_state_num = postfix_exp.size() * 2;
         // 构建能容纳字符长度的NFA
@@ -190,7 +198,7 @@ public:
         for (int i = 0; i < postfix_exp.size(); i++) {
             char current_char = postfix_exp[i];
             cout << "current char:" << current_char << endl;
-            if (is_character(current_char)) {
+            if (is_character(current_char) || current_char == '.') {
                 // 如果是字符，构建子NFA，并将其NFA的节点索引入栈
                 EdgeNode *edge = new EdgeNode(counter + 1, current_char);
                 VertexNode basic_node_start = {.index=counter++, .next_edge=edge};
@@ -247,21 +255,124 @@ public:
                 // 添加到图中
                 nfa->Graph[left_opt.second].next_edge = new EdgeNode(right_opt.first, '^');
                 assist.push(make_pair(left_opt.first, right_opt.second));
+            } else if (current_char == '?') {
+                // ? 的构建规则：如果为 ?，弹出栈内一个元素 N(s)，构建 N(r) 将其入栈（r = s?）
+                // ? 为0次或1次，只需要添加一个空串的转换即可
+                // 注意对于扩展的正则操作符，可能不会符合每次都会构建出两个状态的规律（可能不构建状态）
+                auto opt = assist.top();
+                // 这里不需要出栈，因为仅仅添加一条边
+                EdgeNode *edge = new EdgeNode(opt.second, '^');
+                // 添加一条边，插入到邻接表链表末尾
+                EdgeNode *current_edge = nfa->Graph[opt.first].next_edge;
+                while (current_edge->next != nullptr) {
+                    current_edge = current_edge->next;
+                }
+                current_edge->next = edge;
+            } else if (current_char == '+') {
+                // + 的构建规则：如果为 +，弹出栈内一个元素 N(s)，构建 N(r) 将其入栈（r = s+）
+                auto opt = assist.top();
+                // 同样不需要出栈，需要添加两条边（首尾互相连接）
+                EdgeNode *edge = new EdgeNode(opt.second, '^');
+                // 添加一条边，插入到邻接表链表末尾
+                EdgeNode *current_edge = nfa->Graph[opt.first].next_edge;
+                while (current_edge->next != nullptr) {
+                    current_edge = current_edge->next;
+                }
+                // 首连尾
+                current_edge->next = edge;
+                // 尾连首
+                nfa->Graph[opt.second].next_edge = new EdgeNode(opt.first, '^');;
             }
         }
+        // 栈中应该只有一个元素，这就是nfa的起始状态和终止状态
+        auto status = assist.top();
+        nfa->start = status.first;
+        nfa->end = status.second;
+        // 初始化seen_char字段
+        nfa->seen_char = "";
+        // 获取正则出现的所有字符
+        set<char> seen_char = result.second;
+        for (auto it = seen_char.begin(); it != seen_char.end(); it++) {
+            nfa->seen_char += *it;
+        }
+        // 末尾添加空串
+        nfa->seen_char += '^';
+
         return nfa;
+    }
+
+    void show_nfa(NFA *nfa) {
+        // 输出矩阵
+        vector<int> result_matrix[nfa->end + 1][nfa->seen_char.size()];
+        // 列字符索引映射
+        unordered_map<char, int> column_index_map;
+        for (int i = 0; i < nfa->seen_char.size(); i++) {
+            column_index_map[nfa->seen_char[i]] = i;
+        }
+        // 已遍历的节点保存，防止重复遍历
+        set<int> dup_check;
+        // 辅助栈记录需要遍历的起始索引
+        stack<int> iter_start_status;
+        iter_start_status.push(nfa->start);
+        while (!iter_start_status.empty()) {
+            int current_index = iter_start_status.top();
+            iter_start_status.pop();
+
+            if (nfa->Graph[current_index].next_edge != nullptr) {
+                EdgeNode *next_e = nfa->Graph[current_index].next_edge;
+                result_matrix[current_index][column_index_map[next_e->data]].push_back(next_e->adjvex);
+
+                if (dup_check.find(next_e->adjvex) == dup_check.end())
+                    // 如果之前没有遍历过，则将节点入栈
+                    iter_start_status.push(next_e->adjvex);
+                while (next_e->next != nullptr) {
+                    next_e = next_e->next;
+                    result_matrix[current_index][column_index_map[next_e->data]].push_back(next_e->adjvex);
+                    if (dup_check.find(next_e->adjvex) == dup_check.end())
+                        // 如果之前没有遍历过，则将节点入栈
+                        iter_start_status.push(next_e->adjvex);
+                }
+            }
+            dup_check.insert(current_index);
+        }
+        cout << "状态数：" << nfa->end+1 << "\t";
+        cout << "开始状态：" << nfa->start << "\t" << "接受状态：" << nfa->end << endl;
+        cout <<setw(4);
+        for (int i = 0; i < nfa->seen_char.size(); i++) {
+            cout << nfa->seen_char[i] <<setw(8);
+        }
+        cout << endl;
+        for (int i = 0; i < nfa->end + 1; i++) {
+            cout<<left<<setw(4)<<i;
+            for (int j = 0; j < nfa->seen_char.size(); j++) {
+                cout << '{';
+                for (auto it = result_matrix[i][j].begin(); it != result_matrix[i][j].end(); it++) {
+                     cout<< *it;
+                     if(it+1!=result_matrix[i][j].end())
+                         cout <<  ',';
+                }
+                cout << '}';
+                cout<<right<<setw(4);
+            }
+            cout << endl;
+        }
     }
 
 };
 
 int main() {
     // 正则表达式支持：字母、数字、下划线，特殊字符. + ? * | ，小括号()
-    // b&b+
-    string re = "(a|b)*abb";
+    // 定义 ^ 代表空串 & 代表连接
+    // . 在构建nfa状态转换图时，直接视作普通字符
+    // + `a+` 可以转换为 `aa*`
+    // ? `a?` 可以转换为 `(a|^)`
+    string re = "(a|b)*abb+";
 
 
     NFATools tools = NFATools();
-    tools.construct(re);
+    NFA *result = tools.construct(re);
+    tools.show_nfa(result);
+
     return 0;
 
     // leetcode:https://leetcode.cn/problems/Valid-Number/
